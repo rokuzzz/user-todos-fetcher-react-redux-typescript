b2ae13fb159004621694594276ca20725f316b34
Roman Kuzero <rombeldan@gmail.com>
2022-07-03 17:44:18 +0300

    add functionality to switch between pages

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,26 +1,45 @@
-import React, { useEffect } from 'react';
-import { useActions } from '../hooks/useActions';
-import { useTypedSelector } from '../hooks/useTypedSelectors';
+import React, { useEffect } from "react";
+import { useActions } from "../hooks/useActions";
+import { useTypedSelector } from "../hooks/useTypedSelectors";
 
 const TodoList: React.FC = () => {
-  const {todos, loading, error, page, limit} = useTypedSelector(state => state.todo)
-  const {fetchTodos} = useActions()
+  const { todos, loading, error, page, limit } = useTypedSelector(
+    (state) => state.todo
+  );
+  const { fetchTodos, setTodoPage } = useActions();
+  const pages = [1, 2, 3, 4, 5];
 
   useEffect(() => {
-    fetchTodos(page, limit)
-  }, [])
+    fetchTodos(page, limit);
+  }, [page]);
 
   if (loading) {
-    return <h1>Loading in progress...</h1>
+    return <h1>Loading in progress...</h1>;
   }
   if (error) {
-    return <h1>{error}</h1>
+    return <h1>{error}</h1>;
   }
 
   return (
     <div>
-      {todos.map(todo => 
-        <div key={todo.id}>{todo.id} - {todo.title}</div>)}
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          {todo.id} - {todo.title}
+        </div>
+      ))}
+      <div style={{display: "flex"}}>
+        {pages.map((p) => (
+          <div
+            onClick={() => setTodoPage(p)}
+            style={{
+              border: p === page ? "2px solid green" : "1px solid gray",
+              padding: 10,
+            }}
+          >
+            {p}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
